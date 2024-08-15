ee4e0b9cc7cb38db8655003ee6f2d9ae2b7e43f1
Victor Ochoa <victor.silva@luizalabs.com>
2024-08-14 23:17:19 -0300

    feat: add viewmodel

---
+++ src/OMG.Domain/ViewModels/PedidoCard.cs
@@ -0,0 +1,11 @@
+﻿namespace OMG.Domain.ViewModels
+{
+    public class PedidoCard
+    {
+        public int ProdutoId { get; set; }
+        public string NomeCliente { get; set; } = string.Empty;
+        public int TotalItens { get; set; }
+        public DateOnly DataEntrega { get; set; }= new DateOnly();
+        public float ValorTotal { get; set; }
+    }
+}
---
+++ src/OMG.Domain/ViewModels/PedidoItemModal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMG.Domain.ViewModels
+{
+    public class PedidoItemModal
+    {
+        public int ItemId { get; set; }
+        public int Quantidade { get; set; }
+        public string Produto { get; set; } = string.Empty;
+        public string Formato { get; set; } = string.Empty;
+        public string Cor { get; set; } = string.Empty;
+        public string MyProperty { get; set; } = string.Empty;
+    }
+}
