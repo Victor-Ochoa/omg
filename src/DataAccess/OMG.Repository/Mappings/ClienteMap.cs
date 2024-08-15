31ec9a6701c6b02c5b7a221681c6474cb6b84bfe
Victor Ochoa <victor.silva@luizalabs.com>
2024-08-15 16:48:12 -0300

    feat: init repo

---
+++ src/DataAccess/OMG.Repository/Mappings/ClienteMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OMG.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMG.Repository.Mappings
+{
+    public class ClienteMap : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(150);
+            builder.Property(x => x.Telefone).HasMaxLength(50);
+            builder.Property(x => x.Endereco).HasMaxLength(300);
+
+            builder.HasIndex(x => x.Nome);
+        }
+    }
+}
