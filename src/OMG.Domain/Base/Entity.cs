878644c5b9b05b8aff193ea42cd7e017a13b892d
Victor Ochoa <victor.silva@luizalabs.com>
2024-10-17 10:32:17 -0300

    fix: teste commit on main

--- src/OMG.Domain/Base/Entity.cs
+++ src/OMG.Domain/Base/Entity.cs
@@ -7,4 +7,5 @@ public abstract class Entity : ISoftDeletable
     public virtual int Id { get; set; }
     public bool IsDeleted { get; set; } = false;
     public DateTime? DeletedAt { get; set; } = null;
+
 }
