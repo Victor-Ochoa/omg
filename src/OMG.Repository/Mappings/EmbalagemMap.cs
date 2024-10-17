0749fabdeb608c823d8a8853b2f5579a813234a1
Victor Ochoa <victor.silva@luizalabs.com>
2024-10-17 11:26:13 -0300

    feat: add prop on repository

---
+++ src/OMG.Repository/Mappings/EmbalagemMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OMG.Domain.Entities;
+
+namespace OMG.Repository.Mappings;
+
+public class EmbalagemMap : IEntityTypeConfiguration<Embalagem>
+{
+    public void Configure(EntityTypeBuilder<Embalagem> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Descricao).HasMaxLength(250).IsRequired();
+
+        builder.HasIndex(i => i.Descricao);
+
+        builder.HasQueryFilter(x => !x.IsDeleted);
+        builder.HasIndex(i => i.IsDeleted)
+            .HasFilter("IsDeleted = 0");
+    }
+}
