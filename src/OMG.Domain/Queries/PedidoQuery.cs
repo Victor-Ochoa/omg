f73270b2f448adf976e7aae19125142eed6116f7
Victor Ochoa <victor.silva@luizalabs.com>
2024-10-25 09:52:05 -0300

    feat use query file

---
+++ src/OMG.Domain/Queries/PedidoQuery.cs
@@ -0,0 +1,24 @@
+﻿using OMG.Domain.Entities;
+using OMG.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMG.Domain.Queries;
+
+public static class PedidoQuery
+{
+    public static Expression<Func<Pedido, bool>> GetPedidoExcludePedidoStatus(EPedidoStatus ExcludeStatus)
+    {
+        return x => x.Status != ExcludeStatus;
+    }
+
+    public static Expression<Func<Pedido, bool>> GetPedidoWherePedidoStatusEqualAndDataEntregaMenorQue(EPedidoStatus Status, int DataEntregaMenorQue)
+    {
+        return x => x.Status == Status && 
+            x.DataEntrega >= DateOnly.FromDateTime(DateTime.Now).AddDays(-DataEntregaMenorQue);
+    }
+}
--- src/OMG.Repository/Repositories/PedidoRepository.cs
+++ src/OMG.Repository/Repositories/PedidoRepository.cs
@@ -3,6 +3,7 @@ using OMG.Domain.Base.Contract;
 using OMG.Domain.Contracts.Repository;
 using OMG.Domain.Entities;
 using OMG.Domain.Enum;
+using OMG.Domain.Queries;
 
 namespace OMG.Repository.Repositories;
 
@@ -34,7 +35,6 @@ internal class PedidoRepository(OMGDbContext context, IRepositoryEntity<Pedido> 
 
     public async Task<IList<Pedido>> GetPedidosViewHome(int diasExcluirProntos = 14) => await PedidoViewHomeQuery(diasExcluirProntos).ToListAsync();
     private IQueryable<Pedido> PedidoViewHomeQuery(int diasExcluirProntos = 14)
-        => _context.Pedidos.Where(x => x.Status != EPedidoStatus.Entregue)
-                           .Union(_context.Pedidos.Where(x =>
-                            x.Status == EPedidoStatus.Entregue && x.DataEntrega >= DateOnly.FromDateTime(DateTime.Now).AddDays(-diasExcluirProntos)));
+        => _context.Pedidos.Where(PedidoQuery.GetPedidoExcludePedidoStatus(EPedidoStatus.Entregue))
+                           .Union(_context.Pedidos.Where(PedidoQuery.GetPedidoWherePedidoStatusEqualAndDataEntregaMenorQue(EPedidoStatus.Entregue, diasExcluirProntos)));
 }
